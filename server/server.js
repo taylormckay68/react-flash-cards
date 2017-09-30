e5f814936d2c60b017f47bad7772c37cf1beeff5
Taylor McKay <taylormckay68@gmail.com>
2017-09-30 15:34:30 -0600

    added auth0

--- server/server.js
+++ server/server.js
@@ -23,6 +23,57 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db);
+}).catch(err => {
+    console.log('DB connect error >> ', err)
+})
+
+passport.use(new Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    callbackURL: '/auth/callback'
+},
+function (accessToken, refreshToken, extraParams, profile, done) {
+    let db = app.get('db');
+    db.getUserByAuthId([profile.id]).then((response) => {
+        user = response[0];
+        if (!user) {
+            console.log('CREATING USER');
+            db.createUserByAuth([profile.id, profile.displayName]).then((user) => {
+                return done(null, user)
+            })
+        } else {
+            console.log('FOUND USER', user);
+            return done(null, user)
+        }
+    }).catch(err => {
+        console.log(err)
+    });
+}
+));
+
+passport.serializeUser(function(user, done) {
+    console.log('deserializing user: ', user)
+    done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+    console.log('serializing user: ', user)
+    done(null, user)
+})
+
+app.get('/api/login', passport.authenticate('auth0'));
+
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/profile',
+    failureRedirect: '/'
+}),
+function (req, res) {
+    res.status(200).send(req.user);
+});
+
 
 
 
